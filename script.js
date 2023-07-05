8f9ab1b08644079bf5359e5dfc5f199b61b032f6
winniewambui <winniebwambui7@gmail.com>
2023-07-05 16:49:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggleBtn = document.querySelector(".sidebar-togglebtn");
+const closeBtn = document.querySelector(".closing-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+  // if (sidebar.classList.contains("show-sidebar")) {
+  //   sidebar.classList.remove("show-sidebar");
+  // } else {
+  //   sidebar.classList.add("show-sidebar");
+  // }
+  sidebar.classList.toggle("show-sidebar");
+  toggleBtn.classList.toggle('active');
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar");
+});
